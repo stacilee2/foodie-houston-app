08d30eeefeb2946a9fcd656b2156af3b9c4f5d9b
Staci Rosales <stacileep2@gmail.com>
2023-08-09 13:52:41 -0500

    updated models

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,8 +1,8 @@
 class Restaurant < ApplicationRecord
-    has_many :users, through: :reservations
-
     validates :name, presence: true
     validates :cuisine, presence: true
     validates :description, presence: true
     validates :image_url, presence: true
+
+    has_many :users, through: :reservations
 end
